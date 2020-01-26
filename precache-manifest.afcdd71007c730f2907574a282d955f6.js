8cc490e9beb2c33a21be3f1a8579f7d2061be04d
Marcin Koziol <marcin_koziol@outlook.com>
2020-01-26 16:45:52 +0100

    Updates

---
+++ precache-manifest.afcdd71007c730f2907574a282d955f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6497a98fb03c85df3df15b2aab48d1",
+    "url": "/github.io/btc-rate/index.html"
+  },
+  {
+    "revision": "c9c9df5130d89d479421",
+    "url": "/github.io/btc-rate/static/css/main.bf086888.chunk.css"
+  },
+  {
+    "revision": "8b6c50c75eb29e36aa54",
+    "url": "/github.io/btc-rate/static/js/2.c11b8d36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/btc-rate/static/js/2.c11b8d36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9c9df5130d89d479421",
+    "url": "/github.io/btc-rate/static/js/main.f438fb1f.chunk.js"
+  },
+  {
+    "revision": "9209f22b33218ca84166",
+    "url": "/github.io/btc-rate/static/js/runtime-main.52f3411e.js"
+  }
+]);
